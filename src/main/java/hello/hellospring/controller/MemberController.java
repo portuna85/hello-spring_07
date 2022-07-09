d1f63d889a1b226a987003b69929eeb6ad881d84
portuna85 <portuna85@gmail.com>
2022-07-09 11:18:34 +0900

    Spring BASIC

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
-
 }
